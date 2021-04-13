94e2d778ff4eab8e45573406e6a2dfb9ab46a68c
NiZeR <nimishm27@gmail.com>
2021-04-13 13:16:11 +0530

    Adding authentiction

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'c392be6c751bdc3cef6cfc5c'
 db = SQLAlchemy(app)
 
 from market import routes
